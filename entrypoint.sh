df17f933d4dc9b52e920442275df727bc47ae8ab
Martin <martin@kukolos.sk>
2021-11-02 14:59:41 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 ls -la "$4"
 
-lftp -v -c "open -u ${2},${3} ${1}; set ssl:verify-certificate no; set ftp:ssl-allow no; mirror -R ${4} ."
+lftp -c "open -u $2,$3 $1; set ssl:verify-certificate no; set ftp:ssl-allow no; mirror -R $4 ."
