3d8794992d1e39126a40de86f0293963f922e244
Martin <martin@kukolos.sk>
2021-11-02 15:06:17 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
-#!/bin/sh
+#!/bin/sh -e
 
-ls -la "$4"
+echo "TARGET SERVER: $INPUT_SERVER"
+echo "USER: $INPUT_USER"
+echo "SWOURCE_DIRECTORY: $INPUT_SOURCE_FOLDER"
 
-lftp -c "open -u $2,$3 $1; set ssl:verify-certificate no; set ftp:ssl-allow no; mirror -R $4 ."
+echo "==== SOURCE DATA ===="
+ls -la "$INPUT_SOURCE_FOLDER"
+
+echo "==== UPLOADING TO SERVER ===="
+lftp -c "open -u $INPUT_PASSWORD,$INPUT_USER $INPUT_SERVER; set ssl:verify-certificate no; set ftp:ssl-allow no; mirror -R $INPUT_SOURCE_FOLDER ."
